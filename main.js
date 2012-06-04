87cbfbc0769c2ec482a59964d35f24bd42d06884
devona <dward1289@fullsail.edu>
2012-06-04 11:45:30 -0400

    Drop-down box added through JS

--- main.js
+++ main.js
@@ -6,10 +6,24 @@ window.addEventListener("DOMContentLoaded", function(){
 		var theElement = document.getElementById(n);
 		return theElement;
 	}
-	
+	//Create select field element and populate with options.
+	var makeDrop = function (){
+		var formTag = document.getElementsByTagName("form"); //fromTag is array
+		var selectList = elId("select");
+		var makeSelect = document.createElement("select");
+		makeSelect.setAttribute("id", "priorities");
+	for(var i=0, p=priorityGroup.length; i<p; i++) {
+		var makeOption = document.createElement("option");
+		var optText = priorityGroup[i];
+		makeOption.setAttribute("value", optText);
+		makeOption.innerHTML = optText;
+		makeSelect.appendChild(makeOption);
+		}
+		selectList.appendChild(makeSelect);
+	}
 	//Variable defaults
 	var priorityGroup = ["High","Medium","Low"];
-	
+	makeDrop();
 	
 	//Set Link & Submit Click Events
 	var displayLink = elId("displayData");
