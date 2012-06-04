53e10db468f3d972156699a0e8b3ac6945f02a49
devona <dward1289@fullsail.edu>
2012-06-04 14:32:11 -0400

    getData function added.

--- main.js
+++ main.js
@@ -21,6 +21,78 @@ window.addEventListener("DOMContentLoaded", function(){
 		}
 		selectList.appendChild(makeSelect);
 	}
+	
+	//Find value of selected checkbox.
+	var checkedbox = function () {
+	if(elId("home").checked) {
+		categoryValue = elId("home").value;
+		}
+		else {
+			categoryValue = "No"
+			}
+	if(elId("business").checked) {
+		categoryValue = elId("business").value;
+		}
+		else {
+			categoryValue = "No"
+			}
+	if(elId("school").checked) {
+		categoryValue = elId("school").value;
+		}
+		else {
+			categoryValue = "No"
+			}
+	}
+	
+			
+	
+	//Store data function
+	var storeData = function () {
+		var id = Math.floor(Math.random()*10000000001);
+		//Get all form field values and store in object
+		//Object properties contain array w/from label and input value
+		checkedbox();
+		var item = {};
+		item.name = ["Name of Task: ", elId("taskName").value];
+		item.category = ["Category: ", categoryValue];
+		item.priorityLevel = ["Priority: ", elId("priorities").value];
+		item.startUp = ["Starting Date of Task: ", elId("taskDate").value];
+		item.ending = ["Ending Date of Task: ", elId("taskEnd").value];
+		item.alertOption = ["Type of Alert: ", elId("alertWay").value];
+		item.note = ["Notes", elId("notes").value];
+	
+		//Save data into Local Storage: stringify to convert object to string
+		localStorage.setItem(id, JSON.stringify(item));
+		alert("Task Saved!");
+	}
+	
+	//Get data function
+	var getData = function () {
+		//Write data from local storage to browser
+		var makeDiv = document.createElement("div");
+		makeDiv.setAttribute("id", "items");
+		var makeList = document.createElement("ul");
+		makeDiv.appendChild(makeList);
+		document.body.appendChild(makeDiv);
+		for(var i=0, len=localStorage.length; i<len; i++) {
+		var makeLi = document.createElement("li");
+		makeList.appendChild(makeLi);
+		var key = localStorage.key(i);
+		var value = localStorage.getItem(key);
+		//Convert string from local to object
+		var obj = JSON.parse(value);
+		var makeSubList = document.createElement("ul");
+		makeLi.appendChild(makeSubList);
+		for(var r in obj) {
+			var makeSubLi = document.createElement("li");
+			makeSubList.appendChild(makeSubLi);
+			var optSubText = obj[r][0]+" "+obj[r][1];
+			makeSubLi.innerHTML = optSubText;
+			}
+		}
+	}
+	
+	
 	//Variable defaults
 	var priorityGroup = ["High","Medium","Low"];
 	makeDrop();
@@ -29,8 +101,9 @@ window.addEventListener("DOMContentLoaded", function(){
 	var displayLink = elId("displayData");
 	displayLink.addEventListener("click", getData);
 	var clearLink = elId("clear");
-	clearLink.addEventListener("click", clearLocal);
+	//clearLink.addEventListener("click", clearLocal);
 	var submit = elId("submit");
 	submit.addEventListener("click", storeData);
+	var categoryValue = "No";
 	
 });
